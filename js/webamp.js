d5b66499776ac768dfc89d532f4ebfa37b7057c5
ARc <atandarokib@gmail.com>
2023-12-23 09:45:26 -0800

    updated

---
+++ js/webamp.js
@@ -0,0 +1,5 @@
+// Moves the preloader to the top left corner after a delay
+setTimeout(() => {
+  document.querySelector(".preloader").style.animation =
+    "moveToTopLeft 2s forwards";
+}, 3000);
